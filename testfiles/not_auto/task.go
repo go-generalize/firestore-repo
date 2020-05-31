dfe473d0305b5832e3b0311aaa845dd519454ff1
54mch4n <54mch4n.1111@gmail.com>
2020-05-31 23:28:08 +0900

    tests: various tests

---
+++ testfiles/not_auto/task.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"time"
+)
+
+//go:generate firestore-repo Task
+
+// Task ID自動生成なし
+type Task struct {
+	Identity   string    `firestore:"-" firestore_key:""`
+	Desc       string    `firestore:"description"`
+	Created    time.Time `firestore:"created"`
+	Done       bool      `firestore:"done"`
+	Done2      bool      `firestore:"done2"`
+	Count      int       `firestore:"count"`
+	Count64    int64     `firestore:"count64"`
+	NameList   []string  `firestore:"nameList"`
+	Proportion float64   `firestore:"proportion"`
+	Flag       Flag      `firestore:"flag"`
+}
+
+type Flag bool
---
+++ testfiles/not_auto/tests/model_test.go
@@ -0,0 +1,498 @@
+// +build internal
+
+package tests
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/firestore"
+	model "github.com/go-generalize/firestore-repo/testfiles/not_auto"
+)
+
+func initFirestoreClient(t *testing.T) *firestore.Client {
+	t.Helper()
+
+	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
+		os.Setenv("FIRESTORE_EMULATOR_HOST", "localhost:8000")
+	}
+
+	os.Setenv("FIRESTORE_PROJECT_ID", "project-id-in-google")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := firestore.NewClient(ctx, "testing2")
+
+	if err != nil {
+		t.Fatalf("failed to initialize firestore client: %+v", err)
+	}
+
+	return client
+}
+
+func compareTask(t *testing.T, expected, actual *model.Task) {
+	t.Helper()
+
+	if actual.Identity != expected.Identity {
+		t.Fatalf("unexpected identity: %s (expected: %s)", actual.Identity, expected.Identity)
+	}
+
+	if !actual.Created.Equal(expected.Created) {
+		t.Fatalf("unexpected time: %s(expected: %s)", actual.Created, expected.Created)
+	}
+
+	if actual.Desc != expected.Desc {
+		t.Fatalf("unexpected desc: %s(expected: %s)", actual.Desc, expected.Created)
+	}
+
+	if actual.Done != expected.Done {
+		t.Fatalf("unexpected done: %v(expected: %v)", actual.Done, expected.Done)
+	}
+}
+
+func TestFirestoreTransactionTask(t *testing.T) {
+	client := initFirestoreClient(t)
+
+	taskRepo := model.NewTaskRepository(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	var ids []string
+	defer func() {
+		defer cancel()
+		if err := taskRepo.DeleteMultiByIdentities(ctx, ids); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	now := time.Unix(0, time.Now().UnixNano())
+	desc := "Hello, World!"
+
+	t.Run("Multi", func(tr *testing.T) {
+		tks := make([]*model.Task, 0)
+		for i := int64(1); i <= 10; i++ {
+			tk := &model.Task{
+				Identity:   fmt.Sprintf("Task_%d", i),
+				Desc:       fmt.Sprintf("%s%d", desc, i),
+				Created:    now,
+				Done:       true,
+				Done2:      false,
+				Count:      int(i),
+				Count64:    0,
+				Proportion: 0.12345 + float64(i),
+				NameList:   []string{"a", "b", "c"},
+				Flag:       model.Flag(true),
+			}
+			tks = append(tks, tk)
+		}
+		idList, err := taskRepo.InsertMulti(ctx, tks)
+		if err != nil {
+			tr.Fatalf("%+v", err)
+		}
+		ids = append(ids, idList...)
+
+		tks2 := make([]*model.Task, 0)
+		for i := int64(1); i <= 10; i++ {
+			tk := &model.Task{
+				Identity:   ids[i-1],
+				Desc:       fmt.Sprintf("%s%d", desc, i),
+				Created:    now,
+				Done:       true,
+				Done2:      false,
+				Count:      int(i),
+				Count64:    i,
+				Proportion: 0.12345 + float64(i),
+				NameList:   []string{"a", "b", "c"},
+				Flag:       model.Flag(true),
+			}
+			tks2 = append(tks2, tk)
+		}
+		if err := taskRepo.UpdateMulti(ctx, tks2); err != nil {
+			tr.Fatalf("%+v", err)
+		}
+
+		if tks[0].Identity != tks2[0].Identity {
+			tr.Fatalf("unexpected identity: %s (expected: %s)", tks[0].Identity, tks2[0].Identity)
+		}
+	})
+
+	t.Run("Single", func(tr *testing.T) {
+		tk := &model.Task{
+			Identity:   "Single",
+			Desc:       fmt.Sprintf("%s%d", desc, 1001),
+			Created:    now,
+			Done:       true,
+			Done2:      false,
+			Count:      11,
+			Count64:    11,
+			Proportion: 0.12345 + 11,
+			NameList:   []string{"a", "b", "c"},
+			Flag:       model.Flag(true),
+		}
+		id, err := taskRepo.Insert(ctx, tk)
+		if err != nil {
+			tr.Fatalf("%+v", err)
+		}
+		ids = append(ids, id)
+
+		tk.Count = 12
+		if err := taskRepo.Update(ctx, tk); err != nil {
+			tr.Fatalf("%+v", err)
+		}
+
+		tsk, err := taskRepo.Get(ctx, tk.Identity)
+		if err != nil {
+			tr.Fatalf("%+v", err)
+		}
+
+		if tsk.Count != 12 {
+			tr.Fatalf("unexpected Count: %d (expected: %d)", tsk.Count, 12)
+		}
+	})
+}
+
+func TestFirestoreListTask(t *testing.T) {
+	client := initFirestoreClient(t)
+
+	taskRepo := model.NewTaskRepository(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	var ids []string
+	defer func() {
+		defer cancel()
+		if err := taskRepo.DeleteMultiByIdentities(ctx, ids); err != nil {
+			t.Fatalf("%+v\n", err)
+		}
+	}()
+
+	now := time.Unix(0, time.Now().UnixNano())
+	desc := "Hello, World!"
+
+	tks := make([]*model.Task, 0)
+	for i := 1; i <= 10; i++ {
+		tk := &model.Task{
+			Identity:   fmt.Sprintf("%d", i),
+			Desc:       fmt.Sprintf("%s%d", desc, i),
+			Created:    now,
+			Done:       true,
+			Done2:      false,
+			Count:      i,
+			Count64:    0,
+			Proportion: 0.12345 + float64(i),
+			NameList:   []string{"a", "b", "c"},
+			Flag:       model.Flag(true),
+		}
+		tks = append(tks, tk)
+	}
+	ids, err := taskRepo.InsertMulti(ctx, tks)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+
+	t.Run("int(1件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			Count: model.NumericCriteriaBase.Parse(1),
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 1 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("float(1件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			Proportion: model.NumericCriteriaBase.Parse(1.12345),
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 1 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("bool(10件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			Done: model.BoolCriteriaTrue,
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("time.Time(10件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			Created: now,
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("[]string(10件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			NameList: []string{"a", "b"},
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("Flag(10件)", func(t *testing.T) {
+		req := &model.TaskListReq{
+			Flag: model.BoolCriteriaTrue,
+		}
+
+		tasks, err := taskRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+}
+
+/* TODO Map版Indexes実装
+func TestFirestoreListNameWithIndexes(t *testing.T) {
+	client := initFirestoreClient(t)
+
+	nameRepo := model.NewNameRepository(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	var ids []string
+	defer func() {
+		defer cancel()
+		if err := nameRepo.DeleteMultiByIDs(ctx, ids); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	now := time.Unix(0, time.Now().UnixNano())
+	desc := "Hello, World!"
+	desc2 := "Prefix, Test!"
+
+	tks := make([]*model.Name, 0)
+	for i := int64(1); i <= 10; i++ {
+		tk := &model.Name{
+			Identity:        i,
+			Created:   now,
+			Desc:      fmt.Sprintf("%s%d", desc, i),
+			Desc2:     fmt.Sprintf("%s%d", desc2, i),
+			Done:      true,
+			Count:     int(i),
+			PriceList: []int{1, 2, 3, 4, 5},
+		}
+		tks = append(tks, tk)
+	}
+	ids, err := nameRepo.InsertMulti(ctx, tks)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+
+	t.Run("int(1件)", func(t *testing.T) {
+		req := &model.NameListReq{
+			Count: model.NumericCriteriaBase.Parse(1),
+		}
+
+		tasks, err := nameRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 1 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("bool(10件)", func(t *testing.T) {
+		req := &model.NameListReq{
+			Done: model.BoolCriteriaTrue,
+		}
+
+		tasks, err := nameRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("like(10件)", func(t *testing.T) {
+		req := &model.NameListReq{
+			Desc: "ll",
+		}
+
+		tasks, err := nameRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("prefix", func(t *testing.T) {
+		t.Run("Success", func(t *testing.T) {
+			req := &model.NameListReq{
+				Desc2: "Pre",
+			}
+
+			tasks, err := nameRepo.List(ctx, req, nil)
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+
+			if len(tasks) != 10 {
+				t.Fatal("not match")
+			}
+		})
+
+		t.Run("Failure", func(t *testing.T) {
+			req := &model.NameListReq{
+				Desc2: "He",
+			}
+
+			tasks, err := nameRepo.List(ctx, req, nil)
+			if err != nil {
+				t.Fatalf("%+v", err)
+			}
+
+			if len(tasks) != 0 {
+				t.Fatal("not match")
+			}
+		})
+	})
+
+	t.Run("time.Time(10件)", func(t *testing.T) {
+		req := &model.NameListReq{
+			Created: now,
+		}
+
+		tasks, err := nameRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+
+	t.Run("[]int(10件)", func(t *testing.T) {
+		req := &model.NameListReq{
+			PriceList: []int{1, 2, 3},
+		}
+
+		tasks, err := nameRepo.List(ctx, req, nil)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+
+		if len(tasks) != 10 {
+			t.Fatal("not match")
+		}
+	})
+}
+*/
+
+func TestFirestoreMock(t *testing.T) {
+	client := initFirestoreClient(t)
+
+	taskRepo := model.NewTaskRepository(client)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	now := time.Unix(time.Now().Unix(), 0)
+	desc := "hello"
+
+	id, err := taskRepo.Insert(ctx, &model.Task{
+		Identity: "TestID",
+		Desc:     desc,
+		Created:  now,
+		Done:     true,
+	})
+
+	if err != nil {
+		t.Fatalf("failed to put item: %+v", err)
+	}
+
+	ret, err := taskRepo.Get(ctx, id)
+
+	if err != nil {
+		t.Fatalf("failed to get item: %+v", err)
+	}
+
+	compareTask(t, &model.Task{
+		Identity: id,
+		Desc:     desc,
+		Created:  now,
+		Done:     true,
+	}, ret)
+
+	returns, err := taskRepo.GetMulti(ctx, []string{id})
+
+	if err != nil {
+		t.Fatalf("failed to get item: %+v", err)
+	}
+
+	if len(returns) != 1 {
+		t.Fatalf("GetMulti should return 1 item: %#v", returns)
+	}
+
+	compareTask(t, &model.Task{
+		Identity: id,
+		Desc:     desc,
+		Created:  now,
+		Done:     true,
+	}, returns[0])
+
+	compareTask(t, &model.Task{
+		Identity: id,
+		Desc:     desc,
+		Created:  now,
+		Done:     true,
+	}, ret)
+
+	if err := taskRepo.DeleteByIdentity(ctx, id); err != nil {
+		t.Fatalf("delete failed: %+v", err)
+	}
+
+	if _, err := taskRepo.Get(ctx, id); err == nil {
+		t.Fatalf("should get an error")
+	}
+}
