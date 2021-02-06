b707a1da5f636987fa50cfebd205e11a09921f1a
54m <30588003+54m@users.noreply.github.com>
2021-02-07 04:30:54 +0900

    Revert "update: exec goimports"
    This reverts commit 3fea784072151e8205184c64c28a3e0f59e03a64.

--- main.go
+++ main.go
@@ -253,19 +253,13 @@ func generate(gen *generator, fs *token.FileSet, structType *ast.StructType) err
 	}
 
 	{
-		fileName := gen.GeneratedFileName + ".go"
-		fp, err := os.Create(fileName)
+		fp, err := os.Create(gen.GeneratedFileName + ".go")
 		if err != nil {
 			panic(err)
 		}
 		defer fp.Close()
 
 		gen.generate(fp)
-
-		_, err = execCommand("goimports", "-w", fileName)
-		if err != nil {
-			log.Fatalf("goimports exec error (%s): %v", fileName, err)
-		}
 	}
 
 	if gen.EnableIndexes {
